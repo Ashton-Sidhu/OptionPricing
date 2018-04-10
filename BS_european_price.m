5a52fcec96dbb04bc953b02d27e805ee8dc195f7
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2018-04-09 23:50:16 -0400

    completed A4

--- BS_european_price.m
+++ BS_european_price.m
@@ -2,10 +2,11 @@ function [ call_price, put_price ] = BS_european_price(S0, K, T, r, sigma)
 
     t = 0;
     expPart = exp(-r*(T-t));
-    d1 = (1/(sigma*sqrt(T-t))) * (log(S0/K) + (r + sigma ^ 2) * (T-t));
+    d1 = log(S0 / K) + (r + sigma^2 / 2) * T;
+    d1 = d1 / (sigma * sqrt(T));
     d2 = d1 - sigma * sqrt(T-t);
     
-    call_price = (normcdf(d1) * S0) - (normcdf(d2) * K * expPart);
-    put_price = (normcdf(-d2) * K * expPart) - (normcdf(-d1) * S0);
+    call_price = normcdf(d1) * S0 - normcdf(d2) * K * expPart;
+    put_price = normcdf(-d2) * K * expPart - normcdf(-d1) * S0;
 end
 
--- MC_european_price.m
+++ MC_european_price.m
@@ -1,7 +1,8 @@
-function [ call_price, put_price ] = MC_european_price(S0, K, T, r, mu, sigma, numSteps, numPaths)
+function [ call_price, put_price, S ] = MC_european_price(S0, K, T, r, mu, sigma, numSteps, numPaths)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
     S = GRWPaths(S0, mu, sigma, T, numSteps, numPaths);
+    
     option_call_price = max(0, S(end, :) - K);
     option_put_price = max(0, K - S(end, :));
     
